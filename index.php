d4fc9617c1d263f66c025ddddf992e3477652614
David <dmonroe@killgorack.com>
2019-04-06 09:37:36 -0400

    test

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 
   include 'includes/header.php';
   include 'includes/functions.php';
-
-  $package_obj = new package;
-  $package = $package_obj->get_package();
-
   include 'includes/switcher.php';
   include 'includes/footer.php';
 
